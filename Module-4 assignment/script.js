ff6f0d4509c20a3b1772ea51b9965421c87ccbbe
dkganesh2002 <113645092+dkganesh2002@users.noreply.github.com>
2022-10-27 19:39:50 +0530

    Update script.js
    Signed-off-by: dkganesh2002 <113645092+dkganesh2002@users.noreply.github.com>

--- Module-4 assignment/script.js
+++ Module-4 assignment/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Dk", "buddy", "Jerin", "Json", "Peter", "Luouis", "sheethal", "jaganath", "Ram", "ahamed"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
     
